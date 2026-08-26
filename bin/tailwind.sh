acb4b5338a5a5490e12fc92d4bc0d926db19a698
marco <email@marcobeierer.com>
2026-08-26 22:00:59 +0200

    add --ignore-scripts to make impl more robuts if .npmrc doesn't take effect

--- bin/tailwind.sh
+++ bin/tailwind.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #npx tailwindcss -i ui/uix/web/tailwind.css -o ui/uix/web/assets/tailwind.css --watch
-npx @tailwindcss/cli -i ui/uix/web/tailwind.css -o ui/uix/web/assets/tailwind.css --watch
+npx --ignore-scripts @tailwindcss/cli -i ui/uix/web/tailwind.css -o ui/uix/web/assets/tailwind.css --watch
